b1a86d3d7d99699a1166d5fd76ae716fa7798d64
BuyseTjorven <tjorven.buyse@student.howest.be>
2023-11-20 12:24:00 +0100

    nieuwe oef

---
+++ uitbreiding/vars.tf
@@ -0,0 +1,15 @@
+variable "vsphere_user" {
+  type = string
+  default = "value"
+}
+
+variable "vsphere_password" {
+  type = string
+}
+variable "ubuntu_password" {
+  type = string
+}
+variable "ubuntu_username" {
+  type = string
+  default = "tjorven"
+}
